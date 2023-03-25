afc62cf5eaca79194300bb2ac2501d8a64096496
Jake Baum <jake.jakebaum.uk>
2023-03-25 15:07:30 +0000

    Created a few endpoints that return data from DB in JSON format

--- recipy_app/urls.py
+++ recipy_app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  path('recipy/', views.recipies, name='recipies'),
+  path('recipy/<int:id>/', views.recipy, name='recipy'),
+
+  path('ingredient/', views.ingredients, name='ingredients'),
+  path('ingredient/<int:id>', views.ingredient, name='ingredient')
 ]
--- recipy_app/views.py
+++ recipy_app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Recipy, Ingredient
 
 # Create your views here.
-def index(request):
-  return HttpResponse("Hello world")
+def recipies(request):
+  recipies = list(Recipy.objects.values())
+  return JsonResponse(recipies, safe=False)
+
+def recipy(request, id):
+  return JsonResponse(get_object_or_404(Recipy, pk=id), safe=False)
+
+
+def ingredients(request):
+  ingredients = Ingredient.objects.values()
+  return JsonResponse(ingredients, safe=False)
+
+def ingredient(request, id):
+  return JsonResponse(get_object_or_404(Ingredient, pk=id), safe=False)
