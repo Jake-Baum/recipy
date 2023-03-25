60de145805e6cd3cac31b31024043abed1cc2301
Jake Baum <jake.jakebaum.uk>
2023-03-25 13:57:37 +0000

    Added some models for Recipy and Ingredient.

--- recipy/settings.py
+++ recipy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipy_app.apps.RecipyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- recipy_app/models.py
+++ recipy_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+class Recipy(models.Model):
+  title = models.CharField(max_length=50)
+  ingredients = models.ManyToManyField(Ingredient)
+
+  def begins_with(self, s):
+    return self.title.startswith(s)
+
+  def __str__(self):
+    return self.title
