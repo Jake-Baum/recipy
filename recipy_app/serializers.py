1857d3317343a58ff2b7fe12c8275863bbc51bae
Jake Baum <jake.jakebaum.uk>
2023-03-26 16:55:10 +0100

    Made ingredient name a unique field, and implemented logic in serializers to use existing ingredients if they already exist

--- recipy_app/serializers.py
+++ recipy_app/serializers.py
@@ -1,16 +1,21 @@
 from django.db.models import fields
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, IntegerField
 from .models import Recipy, Ingredient
 
 
 class IngredientSerializer(ModelSerializer):
+	id = IntegerField(required=False)
 
 	class Meta:
 		model = Ingredient
 		fields = '__all__'
+		extra_kwargs = {
+			'name': {'validators': []}
+		}
 
 
 class RecipySerializer(ModelSerializer):
+	id = IntegerField(required=False)
 	ingredients = IngredientSerializer(many=True)
 
 	class Meta:
@@ -23,9 +28,13 @@ class RecipySerializer(ModelSerializer):
 		recipy = Recipy.objects.create(**validated_data)
 
 		for ingredient in ingredients:
-			new_ingredient = Ingredient.objects.create(**ingredient)
-			new_ingredient.recipies.add(recipy) # type: ignore
-	    
+			try:
+				existing_ingredient = Ingredient.objects.get(id=ingredient['id'])
+				existing_ingredient.recipies.add(recipy) # type: ignore
+			except Ingredient.DoesNotExist:
+				new_ingredient = Ingredient.objects.create(**ingredient)
+				new_ingredient.recipies.add(recipy) # type: ignore
+
 		return recipy
 	
 	def update(self, instance, validated_data):
@@ -34,7 +43,11 @@ class RecipySerializer(ModelSerializer):
 
 		ingredients = validated_data.pop('ingredients')
 		for ingredient in ingredients:
-			new_ingredient = Ingredient.objects.create(**ingredient)
-			new_ingredient.recipies.add(instance) # type: ignore
+			try:
+				existing_ingredient = Ingredient.objects.get(id=ingredient['id'])
+				existing_ingredient.recipies.add(instance) # type: ignore
+			except Ingredient.DoesNotExist:
+				new_ingredient = Ingredient.objects.create(**ingredient)
+				new_ingredient.recipies.add(instance) # type: ignore
 
 		return instance
